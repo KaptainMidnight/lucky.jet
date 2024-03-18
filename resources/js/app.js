eced8018df36b69d342b4f199bdf1d6a1345e8a3
artemkin <aartemkin030@gmail.com>
2024-03-18 23:40:15 +0400

    Changed config for client-side inertia + vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,14 @@
-import {defineConfig} from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from "@vitejs/plugin-vue";
+import {createApp, h} from 'vue'
+import {createInertiaApp} from '@inertiajs/vue3'
 
-export default defineConfig({
-    plugins: [
-        vue(),
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        })
-    ]
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./pages/**/*.vue', {eager: true})
+        return pages[`./pages/${name}.vue`]
+    },
+    setup({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
+            .use(plugin)
+            .mount(el)
+    },
 })
