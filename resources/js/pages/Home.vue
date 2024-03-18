6f0cc1ec19b1cebc1a894f7df40eaf736fa42987
artemkin <aartemkin030@gmail.com>
2024-03-18 23:49:21 +0400

    Finished installation inertia + vue

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    components: {},
+    setup() {
+        return {}
+    }
+})
+</script>
+
+<template>
+    <h1>Hello world</h1>
+</template>
+
+<style scoped>
+
+</style>
